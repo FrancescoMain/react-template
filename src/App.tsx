53a192b1f26d27a5d69c3054416a1fa5441c874b
francescoMain <cesaranofrancescomain@gmail.com>
2023-03-17 15:39:27 +0100

    add link state and passed via props

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
+import { useState } from "react";
 import "./App.css";
 import Navbar from "./components/navbar/Navbar";
 
 function App() {
-  return <Navbar></Navbar>;
+  const [links, setLinks] = useState([
+    { name: "Home", link: "#" },
+    { name: "About", link: "#" },
+    { name: "Services", link: "#" },
+    { name: "Portfolios", link: "#" },
+    { name: "Blog", link: "#" },
+    { name: "Contact", link: "#" },
+  ]);
+
+  return <Navbar links={links}></Navbar>;
 }
 
 export default App;
