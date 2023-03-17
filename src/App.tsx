6a1fa8d2d56e498d138229f1663889da4f24deaf
francescoMain <cesaranofrancescomain@gmail.com>
2023-03-17 22:48:15 +0100

    import jumbo

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import "./App.css";
 import Navbar from "./components/navbar/Navbar";
+import Jumbo from "./components/jumbo/Jumbo";
 
 function App() {
   const [links, setLinks] = useState([
@@ -12,7 +13,12 @@ function App() {
     { name: "Contact", link: "#" },
   ]);
 
-  return <Navbar links={links}></Navbar>;
+  return (
+    <>
+      <Navbar links={links}></Navbar>
+      <Jumbo></Jumbo>
+    </>
+  );
 }
 
 export default App;
