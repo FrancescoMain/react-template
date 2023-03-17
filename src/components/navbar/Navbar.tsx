f0f422b1c5a4e0d716a7eb5e8a2d9deb59811ace
francescoMain <cesaranofrancescomain@gmail.com>
2023-03-17 15:39:38 +0100

    navbar style

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,7 +1,45 @@
-import React from "react";
+import styles from "./Navbar.module.css";
+import { AiOutlineSearch } from "react-icons/Ai";
 
-const Navbar = () => {
-  return <div>navbar</div>;
+interface Link {
+  name: string;
+  link: string;
+}
+
+interface Props {
+  links: Link[];
+}
+
+const Navbar = ({ links }: Props) => {
+  return (
+    <header className={styles.header}>
+      <ul className={styles.list}>
+        <li>
+          <a href="">Logo</a>
+        </li>
+        <div className={styles.listContainer}>
+          {links.map((link, index) => (
+            <li className={styles.li} key={index}>
+              <a href={link.link}>{link.name}</a>
+            </li>
+          ))}
+          <li>
+            <a href="">
+              <AiOutlineSearch
+                className={styles.search}
+                size="40px"
+              ></AiOutlineSearch>
+            </a>
+          </li>
+          <li>
+            <button className={styles.btn} role="button">
+              Let's Talk
+            </button>
+          </li>
+        </div>
+      </ul>
+    </header>
+  );
 };
 
 export default Navbar;
